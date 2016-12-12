d72e870b2b9d20e8708a350151f652fbd0235aa4
shmilee <shmilee.zju@gmail.com>
2016-12-12 22:42:27 +0800

    update apks

--- extra-app/setting-20161212.sh
+++ extra-app/setting-20161212.sh
@@ -4,14 +4,14 @@
 abi=armeabi-v7a
 abi2=armeabi
 
-APK_DIR="./update-20161016"
+APK_DIR="./update-20161212"
 Overlay='NO'
 
 # 无lib冲突 -> system/app/
 # 有lib冲突 -> system/vivo-apps/
-extra_apps=(BaiduMaps::BaiduMaps_Android_9-5-0_1012676b.apk \
+extra_apps=(BaiduMaps::BaiduMaps_Android_9-5-5_1012676b.apk \
     BubbleUPnP::BubbleUPnP-2.6.1.apk \
-    ChinaMobile10086::ChinaMobile10086_v3.2.0.apk \
+    ChinaMobile10086::CM10086_android_V3.5.1_161110.apk \
     ezPDF_Reader::ezPDF_Reader_v2.6.6.1.apk \
     Firefox_Browser::Firefox_Browser_v47.0.apk \
     ForaDictionary::ForaDictionary_v17.3.apk \
@@ -29,7 +29,7 @@ lib_ignore=(libentryexstd.so)
 
 # -> system/vivo-apps/
 extra_vivoapps=(goldendict::GoldenDict-1.6.5-Android-4.4+-free.apk \
-    qqi::QQ_International_v5.1.0.apk \
+    qqi::qq_5.1.1.6062_android.apk \
     MX_Player_Pro::MX_Player_Pro_1.8.4_20160125_AC3_crk.apk)
 
 #################### Setting ####################
--- more-app/app-list.md
+++ more-app/app-list.md
@@ -1,19 +1,19 @@
 ## sd卡/more-app ---手动安装---> data/app/
 
 * [2WeiMa_v4.5.apk](http://android.25pp.com/detail_248799.html)
-* 12306v2.4.apk (官网)
+* 12306v2.5.apk (官网)
 * acfun_portal.apk (官网)
 * Adobe Flash Player 11.1.apk
 * alipay_wap.apk (支付宝官网)
 * ~~Amap_Android_V7.6.6.2059_GuanWang.apk (官网)~~
 * Baicizhan.apk (官网)
 * ~~BaiduMusic-musicsybutton.apk (官网)~~
-* iBiliPlayer-bili.apk (官网)
-* BusManager.apk (官网)
+* Carbada.apk (官网)
 * CloudMusic_official.apk (官网)
 * com.vivo.browser_v4.4.8.apk (自动更新)
 * didi_psngr.apk (官网)
 * DoodleJump_v1.66.apk (longlongago)
+* iBiliPlayer-bili.apk (官网)
 * [ioscompass.apk](http://soft.shouji.com.cn/down/31064.html)
 * ~~Kingdee.Express_3.2.8_328.apk (官网)~~
 * kiwix-1.97.apk (官网)
